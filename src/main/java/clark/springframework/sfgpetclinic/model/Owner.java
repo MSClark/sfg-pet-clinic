c8f4106b52b73d91843d9f53b35f5701a998d9fa
msclark <msclark3@asu.edu>
2019-10-29 14:13:14 -0700

    added pojos. closes #1

---
+++ src/main/java/clark/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package clark.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/clark/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package clark.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
