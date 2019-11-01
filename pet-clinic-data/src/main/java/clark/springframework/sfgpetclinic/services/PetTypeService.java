ff8356a1ad5bd3bfa7ac2de4dde750dd2964539e
msclark <msclark3@asu.edu>
2019-11-01 15:10:46 -0700

    implement pet type service map closes #29

---
+++ pet-clinic-data/src/main/java/clark/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package clark.springframework.sfgpetclinic.services;
+
+import clark.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
