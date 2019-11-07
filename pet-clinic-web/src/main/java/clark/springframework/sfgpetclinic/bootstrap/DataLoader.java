fc54eaa0a8c4be55edc54c09542966f45c48ff11
msclark <msclark3@asu.edu>
2019-11-07 12:11:48 -0700

    added data to bootstrap loaded for visit closes #48

--- pet-clinic-web/src/main/java/clark/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/clark/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package clark.springframework.sfgpetclinic.bootstrap;
 
 import clark.springframework.sfgpetclinic.model.*;
-import clark.springframework.sfgpetclinic.services.OwnerService;
-import clark.springframework.sfgpetclinic.services.PetTypeService;
-import clark.springframework.sfgpetclinic.services.SpecialtyService;
-import clark.springframework.sfgpetclinic.services.VetService;
+import clark.springframework.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -101,5 +100,11 @@ public class DataLoader implements CommandLineRunner {
         brendansPet.setBirthDate(LocalDate.now());
         brendansPet.setName("abby");
         owner2.getPets().add(brendansPet);
+
+        Visit catVisit =  new Visit();
+        catVisit.setPet(brendansPet);
+        catVisit.setLocalDate(LocalDate.now());
+        catVisit.setDescription("meow");
+        visitService.save(catVisit);
     }
 }
